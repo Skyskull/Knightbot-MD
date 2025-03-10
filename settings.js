667b1c2c920bb595fc246285051c1bff87f324a7
Skyskull <163397194+Skyskull@users.noreply.github.com>
2025-03-11 04:13:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '丹zͥนrͣeͫ🖤',
+  author: 'ASMAAAN',
+  botName: "LORD SKY",
+  botOwner: '𝐋𝐎𝐑𝐃 𝐎𝐅 𝐆𝐄𝐍𝐉𝐔𝐓𝐒𝐔 𝐒𝐊𝐘𝐇🖤', // Your name
+  ownerNumber: '917973611393,919779960108', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
